68b14333067b80312ec05d07c3fe4f57e9d17527
Simge <49365807+simgekeser@users.noreply.github.com>
2022-01-07 11:22:26 +0300

    Update Credentials.java

--- app/src/main/java/com/example/mvvmretrofitjavamovieapp/utils/Credentials.java
+++ app/src/main/java/com/example/mvvmretrofitjavamovieapp/utils/Credentials.java
@@ -4,7 +4,7 @@ public class Credentials {
 
     public static final String BASE_URL = "https://api.themoviedb.org";
 
-    public static final String API_KEY = "816b87bbb69c19eb5d706fc10becae47";
+    public static final String API_KEY = "***";
 
 
 }
